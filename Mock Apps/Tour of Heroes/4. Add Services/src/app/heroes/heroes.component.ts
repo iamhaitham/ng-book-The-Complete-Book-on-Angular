80e72eaa04770d6681a76b39adae72169ee1a061
Haitham Estetieh <iamhaitham2011@gmail.com>
2021-09-20 10:46:53 +0300

    Added HeroService and used Dependency Injection to inject it

---
+++ Mock Apps/Tour of Heroes/4. Add Services/src/app/heroes/heroes.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero.interface';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes:Hero[];
+  selectedHero?:Hero;
+
+  constructor(private heroService:HeroService) {
+    this.heroes=[];
+    this.selectedHero={'id':0,'name':""};
+  }
+
+  onSelect(hero:Hero):void{
+    this.selectedHero=hero;
+  }
+
+  getHeroes():void{
+    this.heroes=this.heroService.getHeroes();
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+}
